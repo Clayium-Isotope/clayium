8b580b9a50c355e798091f784a58ddd8d06b027b
t5ugu <tsugu531@gmail.com>
2021-09-01 00:09:05 +0900

    消す予定だし、消したけどビルドのために復活

---
+++ src/main/java/mods/clayium/creativetab/TabClayium.java
@@ -0,0 +1,35 @@
+
+package mods.clayium.creativetab;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.init.Items;
+import net.minecraft.creativetab.CreativeTabs;
+
+import mods.clayium.ElementsClayiumMod;
+
+@ElementsClayiumMod.ModElement.Tag
+public class TabClayium extends ElementsClayiumMod.ModElement {
+    public TabClayium(ElementsClayiumMod instance) {
+        super(instance, 1);
+    }
+
+    @Override
+    public void initElements() {
+        tab = new CreativeTabs("tabclayium") {
+            @SideOnly(Side.CLIENT)
+            @Override
+            public ItemStack getTabIconItem() {
+                return new ItemStack(Items.CLAY_BALL, (int) (1));
+            }
+
+            @SideOnly(Side.CLIENT)
+            public boolean hasSearchBar() {
+                return false;
+            }
+        };
+    }
+    public static CreativeTabs tab;
+}
