fa961b9bacd347280f67f5ebbdd9b49debd2ef60
t5ugu <tsugu0530@outlook.jp>
2023-04-22 13:20:02 +0900

    🐛 インターフェースを通してGUIを見ると、プレイヤーのインベントリがシフトする不具合

--- src/main/java/mods/clayium/machine/Interface/ClayInterface/ClayInterface.java
+++ src/main/java/mods/clayium/machine/Interface/ClayInterface/ClayInterface.java
@@ -1,6 +1,8 @@
 package mods.clayium.machine.Interface.ClayInterface;
 
+import mods.clayium.core.ClayiumCore;
 import mods.clayium.gui.GuiHandler;
+import mods.clayium.item.common.IModifyCC;
 import mods.clayium.machine.ClayContainer.ClayContainer;
 import mods.clayium.machine.EnumMachineKind;
 import mods.clayium.util.JsonHelper;
@@ -8,8 +10,13 @@ import mods.clayium.util.TierPrefix;
 import mods.clayium.util.UtilLocale;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -45,5 +52,20 @@ public class ClayInterface extends ClayContainer {
     public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced) {
         super.addInformation(stack, player, tooltip, advanced);
         UtilLocale.localizeTooltip(tooltip, "tooltip." + this.kind.getRegisterName());
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if (playerIn.getHeldItem(hand).getItem() instanceof IModifyCC) return false;
+
+        if (worldIn.isRemote) return true;
+
+        worldIn.markBlockRangeForRenderUpdate(pos, pos);
+        TileEntityClayInterface tile = (TileEntityClayInterface) worldIn.getTileEntity(pos);
+        if (tile == null || !tile.isSynced()) return false;
+
+        tile.core.getWorld().markBlockRangeForRenderUpdate(tile.core.getPos(), tile.core.getPos());
+        playerIn.openGui(ClayiumCore.instance(), tile.core.getGuiId(), tile.core.getWorld(), tile.core.getPos().getX(), tile.core.getPos().getY(), tile.core.getPos().getZ());
+        return true;
     }
 }
